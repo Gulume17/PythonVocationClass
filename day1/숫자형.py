136f24c501ff7441c424382f395a5968c0e7f432
Kyeongyunhui <yunhui72659147@gmail.com>
2025-01-22 18:48:04 +0900

    day1

---
+++ day1/숫자형.py
@@ -0,0 +1,30 @@
+num1 = 10 # 양의 정수
+num2 = -3 # 음의 정수
+num3 = 0
+print(num1)
+print(num2)
+print(num3)
+
+print(num1 - num2)
+
+num1 = 100
+num3 = 20
+print(num1 - num3)
+
+# int()
+print(int(1.9))
+print(int('100'))
+print(int(True)) # 1을 반환
+print(int(False)) # 0을 반환
+
+print(int(True + 10))
+
+'''
+day1 정리
+1. 파이썬 (인터프리터 설치) -> 터미널로 연산 테스트 해보기
+2. 파이참 설치
+3. 파이참 깃허브 연결
+4. 주석,변수(변수명,표기법)
+5. 숫자형 - 정수형
+'''
+
